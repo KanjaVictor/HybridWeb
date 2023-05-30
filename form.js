19def7d66ce0718c8cc8869e6846c8a9913cf9c5
Kanja Victor <victorkaguri77@gmail.com>
2023-05-30 23:57:59 +0300

    Initial Commit

---
+++ form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+/**
+ *  Components in React return a piece of JSX code that tells what should be rendered on the screen. 
+ *  There are two types of components:
+ *      1)Functional Components - Simply javascript functions.
+ *      2)Class Components - They work with other components in the program
+ * */
+
+class SimpleForm extends Component {
+    
+}
